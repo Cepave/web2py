99a547c503437a367d46bb16b0365996e8ebd4ee
crosserclaws <crosserclaws@gmail.com>
2016-06-29 17:44:08 +0800

    Add multi-host ps in welcome for demo.

---
+++ applications/welcome/controllers/machine.py
@@ -0,0 +1,33 @@
+import docker
+import datetime
+
+HOST_PATH = '/Users/crosserclaws/.docker'
+HOST_FILENAME = '/host.txt'
+items = []
+with open(HOST_PATH + HOST_FILENAME, 'r') as f:
+    for line in f:
+        host, path = line.split()
+        path = HOST_PATH + path[1:]
+        items.append((host, path))
+
+f = lambda path: docker.tls.TLSConfig(
+        client_cert=( path + '/cert.pem', path + '/key.pem'),
+        verify= path + '/ca.pem'
+    )
+
+g = lambda host: 'tcp://{}:2376'.format(host)
+
+def ps():
+    pss = []
+    for host, path in items:
+        client = docker.Client(base_url=g(host), tls=f(path))
+        ps = client.containers(all=True, trunc=True)
+        # Change the time format of Created
+        for container in ps:
+            utime = container['Created']
+            container['Created'] = datetime.datetime.fromtimestamp(
+                int(utime)
+            ).strftime('%Y-%m-%d %H:%M:%S')
+        pss.append((host, ps))
+
+    return dict(pss=pss)
